fc9db8bb6419bf38b5f5e9c0bf3aa02f0a8f29b0
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-05-02 10:29:51 +0100

    remove method-override

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const methodOverride = require("method-override");
 const Jimp = require("jimp");
 
 
